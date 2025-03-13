73ab22af09c36bcf2954e2b7c478eb15352d5495
Bradley Morrison <bradley_morrison@outlook.com>
2025-03-13 18:25:02 +0000

    Added compressed files, Installed Vite compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/Build/*.gz",
+          dest: "dist/Build",
+        },
+        {
+          src: "public/Build/*.br",
+          dest: "dist/Build",
+        },
+      ],
+    }),
+  ],
+  server: {
+    headers: {
+      "Content-Encoding": "gzip", // or 'br' for Brotli
+    },
+  },
+});
